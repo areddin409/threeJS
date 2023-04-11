7214b3a22dfa5453fafe7da39fceac09a60d8a1c
areddin409 <areddin409@gmail.com>
2023-04-10 22:08:28 -0500

    Working on Tabs

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -19,6 +19,32 @@ import {
 const Customizer = () => {
   const snap = useSnapshot(state)
 
+  const [file, setFile] = useState("")
+
+  const [prompt, setPrompt] = useState("")
+  const [generatingImg, setGeneratingImg] = useState(false)
+
+  const [activeEditorTab, setActiveEditorTab] = useState("")
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false
+  })
+
+  //show tab content depending on the active tab
+  const generateTabContent = () => {
+    console.log(activeEditorTab)
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker />
+      case "aipicker":
+        return <AIPicker />
+      default:
+        return null
+    }
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -31,8 +57,13 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab, index) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
@@ -59,7 +90,7 @@ const Customizer = () => {
                 tab={tab}
                 isFilterTab
                 isActiveTab=""
-                handleClick={() => {}}
+                handleClick={() => setActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
